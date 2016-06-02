0e6f13820e90ad0e284a8d4a37ce1eceb685be23
Kimserey Lam <kimserey.lam@gmail.com>
2016-06-02 23:03:06 +0100

    added transition mixin

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,13 @@
+// Sass configuration
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var webserver = require('gulp-webserver');
 
-gulp.task('sass', function () {
-  gulp.src('*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-});
- 
-gulp.task('watch', function () {
-  gulp.watch('*.scss', ['sass']);
+gulp.task('sass', function() {
+    gulp.src('./scss/app.scss')
+        .pipe(sass())
+        .pipe(gulp.dest("./css"))
 });
 
-gulp.task('webserver', function () {
-    gulp.src('app')
-        .pipe(webserver({
-            livereload: true,
-            open: true
-        }));
-});
+gulp.task('default', ['sass'], function() {
+    gulp.watch('./scss/*/*.scss', ['sass']);
+})
