d8219944c91b64e4f23656944087c0c9c204f345
Kimserey Lam <kimserey.lam@gmail.com>
2016-06-08 23:05:57 +0100

    changed app.js to use jquery

--- js/app.js
+++ js/app.js
@@ -1,45 +1,17 @@
-function toggleCardLens(el) {
-    var content = document.getElementById(el.dataset.target);
+function togglePanel() {
+    $('.panel-left, .panel-right').toggleClass('active');
+}
 
-    if(el.classList.contains('expend')) {
-        el.classList.remove('expend');
-        content.classList.remove('expend');
-    } else {
-        el.classList.add('expend');
-        content.classList.add('expend');
-    }
+function toggleCardDetails(el) {
+    $(el).toggleClass('expend');
+    $('#' + el.dataset.target).toggleClass('expend');
 }
 
 function toggleFilter() {
-    var el = document.getElementById('filter');
-
-    if(el.classList.contains('show')) {
-        el.classList.remove('show');
-    } else {
-        el.classList.add('show');
-    }
+    $('#filter').toggleClass('show');
 }
 
 function toggleMenu() {
-    var mask = document.getElementById('mask');
-    var menu = document.getElementById('side-menu');
-    var body = document.body;
-    
-    var show = function () {
-        mask.classList.add('show');
-        menu.classList.add('show');
-        body.classList.add("disable-scroll");
-    }
-    
-    var hide = function () {
-        mask.classList.remove('show');
-        menu.classList.remove('show');
-        body.classList.remove('disable-scroll');
-    }
-    
-    if(mask.classList.contains('show')) {
-        hide();
-    } else {
-        show();
-    }
+    $('#mask, #side-menu').toggleClass('show');
+    $('body').toggleClass('disable-scroll');
 }
